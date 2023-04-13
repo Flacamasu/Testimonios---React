8783d07f2abcd31df14634cacdac84f030f4e181
Luis Araya <luis.27062000@gmail.com>
2023-04-13 17:06:48 -0400

    Se termina el formato de los testimonio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+        <h1>Estas son las opiniones: </h1>
+        <Testimonio 
+          nombre='Ricardo'
+          pais='Chile'
+          imagen='1'
+          titulo='Ingenieria Informatica'
+          empresa='Google'
+          testimonio='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt ex nunc, ut dictum magna fringilla vel. Phasellus sollicitudin sollicitudin dictum. Curabitur faucibus magna magna, sit amet malesuada enim tincidunt non. Praesent tortor urna, pharetra a tincidunt eu, convallis vel nisl. Curabitur vel libero scelerisque, lacinia urna sit amet, sollicitudin sapien. Cras fringilla convallis diam non pretium. Etiam sed mattis risus, ac aliquam est. Nam viverra neque sed pellentesque consectetur. Duis eget faucibus ante, ac gravida massa.'
+        /> 
+        <Testimonio 
+          nombre='Maria'
+          pais='Italia'
+          imagen=''
+          titulo='Psicologia'
+          empresa='UNAB'
+          testimonio='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt ex nunc, ut dictum magna fringilla vel. Phasellus sollicitudin sollicitudin dictum. Curabitur faucibus magna magna, sit amet malesuada enim tincidunt non. Praesent tortor urna, pharetra a tincidunt eu, convallis vel nisl. Curabitur vel libero scelerisque, lacinia urna sit amet, sollicitudin sapien. Cras fringilla convallis diam non pretium. Etiam sed mattis risus, ac aliquam est. Nam viverra neque sed pellentesque consectetur. Duis eget faucibus ante, ac gravida massa.'
+        /> 
+        <Testimonio 
+          nombre='Maria'
+          pais='Italia'
+          imagen='2'
+          titulo='Psicologia'
+          empresa='UNAB'
+          testimonio='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt ex nunc, ut dictum magna fringilla vel. Phasellus sollicitudin sollicitudin dictum. Curabitur faucibus magna magna, sit amet malesuada enim tincidunt non. Praesent tortor urna, pharetra a tincidunt eu, convallis vel nisl. Curabitur vel libero scelerisque, lacinia urna sit amet, sollicitudin sapien. Cras fringilla convallis diam non pretium. Etiam sed mattis risus, ac aliquam est. Nam viverra neque sed pellentesque consectetur. Duis eget faucibus ante, ac gravida massa.'
+        /> 
+        <Testimonio 
+          nombre='Maria'
+          pais='Italia'
+          imagen='1'
+          titulo='Psicologia'
+          empresa='UNAB'
+          testimonio='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt ex nunc, ut dictum magna fringilla vel. Phasellus sollicitudin sollicitudin dictum. Curabitur faucibus magna magna, sit amet malesuada enim tincidunt non. Praesent tortor urna, pharetra a tincidunt eu, convallis vel nisl. Curabitur vel libero scelerisque, lacinia urna sit amet, sollicitudin sapien. Cras fringilla convallis diam non pretium. Etiam sed mattis risus, ac aliquam est. Nam viverra neque sed pellentesque consectetur. Duis eget faucibus ante, ac gravida massa.'
+        /> 
+        <Testimonio 
+          nombre='Maria'
+          pais='Italia'
+          imagen='1'
+          titulo='Psicologia'
+          empresa='UNAB'
+          testimonio='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt ex nunc, ut dictum magna fringilla vel. Phasellus sollicitudin sollicitudin dictum. Curabitur faucibus magna magna, sit amet malesuada enim tincidunt non. Praesent tortor urna, pharetra a tincidunt eu, convallis vel nisl. Curabitur vel libero scelerisque, lacinia urna sit amet, sollicitudin sapien. Cras fringilla convallis diam non pretium. Etiam sed mattis risus, ac aliquam est. Nam viverra neque sed pellentesque consectetur. Duis eget faucibus ante, ac gravida massa.'
+        /> 
+      </div>
+      
     </div>
   );
 }
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import '../hojas-de-estilo/Testimonio.css'
 
-function Testimonio() {
+const imagen = new Image();
+imagen.onload = () => {
+  // La imagen se cargó correctamente
+};
+imagen.onerror = (error) => {
+  console.error('Error al cargar la imagen:', error);
+  // Aquí se podría mostrar un mensaje de error al usuario, o realizar alguna otra acción.
+};
+
+function Testimonio(props) {
+  
+  let imagen;
+  try {
+    imagen = require(`../imagenes/testimonio-${props.imagen}.jpg`);
+  } catch (error) {
+    imagen = require(`../imagenes/no-encontrada.jpg`);
+  }
+
   return(
    <div className='contenedor-testimonio'>
     <img
       className='imagen-testimonio'
-      src={require('../imagenes/testimonio-1.jpg')}
+      src={imagen}
+
+      onError={(e) => {
+        console.error('Error al cargar la imagen:', e.target.src);
+      }}
       alt='Foto de persona 1'
     />
+    <div className='contenedor-texto-testimonio'>
+      <p className='nombre-testimonio'>
+        <strong>{props.nombre}</strong> en {props.pais}
+      </p>
+      <p className='cargo-testimonio'>{props.titulo} en <strong>{props.empresa}</strong></p>
+      <p className='texto-testimonio'>"{props.testimonio}"</p>
+    </div>
    </div>
   );
-} 
+}
+
+export default Testimonio;
