9a7552bd6d1ea37d18e507eb6490536214f060b7
Luis Araya <luis.27062000@gmail.com>
2023-04-13 10:03:38 -0400

    Se agrega componente e imaganes de los testimonios

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Testimonio() {
+  return(
+   <div className='contenedor-testimonio'>
+    <img
+      className='imagen-testimonio'
+      src={require('../imagenes/testimonio-1.jpg')}
+      alt='Foto de persona 1'
+    />
+   </div>
+  );
+} 
